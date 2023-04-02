a6af02b86dd7a57b71382933747ad88b026a4d7d
jhdavey <jhdavey5182@gmail.com>
2023-04-02 16:35:41 -0400

    Fixed recent search buttons to run new search and populate dashboard with updated weather data. Created error messages for when users input numbers or strings that do not match cities. Removed framework styling and improved look of dashboard. Updated README with all relevant info.

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# checktheweather
+# Check the Weather Tool
+
+This is a simple weather tool that allows you to search a city and view the current weather as well as the 5 day forecast.
+
+# What You'll Find
+
+The weather dashboard allows you to search for weather data based on the city entered. You'll see a simple search input form to the left. Once a city is entered, the current weather information and 5 day forecast will populate to the right of the search area.
+
+Additionally once you've completed a search you will see you recent search history list populate under the search bar.
+
+Your page should be displayed as follows:
+
+![Weather Dashboard](/codingquizscreenshot1.png)
+## Description
+
+Simply enter the city you would like to view weather information for.
+
+[Visit the Webpage Here](http://jhdavey.github.io/checktheweather)
+
+## Help
+
+If any issues arise while viewing this webpage please contact Harley Davey (See below for contact info)
+
+## Authors
+
+*Initial Development and Deployment:*
+
+Harley Davey
+
+jhdavey.github.io
+
+## Version History
+* 1.0 as of 4/2/2023
+
+## License
+
+This project is licensed under the MIT License - see the LICENSE.md file for details
+
+## Acknowledgments
+
+Original design and development by HD Web Development / Harley Davey
--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-// GIVEN a weather dashboard with form inputs
-    //Search bar w/ jquiry autofill?
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
-
 var recentCities = [];
 var pulledHistory = [];
 var lat;
 var lon;
 var city = '';
+var historyName = '';
+var historyListItem = '';
 var geocodingUrl = '';
 var weatherDataUrl = '';
 var weatherApiKey = '33de59ca489ef80bcfed5ff29e61912e';
@@ -16,81 +13,114 @@ var showCurrentData = '';
 var currentTemp = '';
 var currentWind = '';
 var currentHumidity = '';
+var dailyTemp = [];
+var dailyWind = [];
+var dailyHumidity = [];
 var searchRun = false;
 var recentSearches = document.getElementById('recent-searches');
 var currentCitySection = document.getElementById('current-city');
 var fiveDayHeader = document.getElementById('fivedayheader');
 var fiveDayList = document.getElementById('fivedaylist');
-//get date from day.js
 var date = dayjs().format('M/DD/YYYY');
 
-//Load recent searches on page load 
-checkSearch();
-
-//Wrap JS in function to start on search submit
-function search() {
-
-        //Get city from search
-        function getCity() {
-            city = document.getElementById('city').value;
-            //TODO TELL USER IF ENTRY IS NOT AN OPTION
-            //Clear search
-            $("#search-form")[0].reset();
-        }
-        getCity();
-
-        //Add city to beginning of recentCities array and save to LocalStorage
-        recentCities.unshift(city);
-        localStorage.setItem('recentCities', JSON.stringify(recentCities));
-        
-        //Openweather API Url based on city search input
-        geocodingUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=1&appid=' + weatherApiKey;
-
-        //Get Lat/Lon from geocoding API
-        function getLatLon() {
-            fetch(geocodingUrl)
-                .then(function (response) {
-                if (response.ok) {
-                    response.json().then(function (data) {
-                    lat = data[0].lat;
-                    lon = data[0].lon;
-                    console.log(data);
-                    getWeatherData();
-                    })
-                } else {
-                        console.log(error);
-                    }
-                })
-        }
-        getLatLon();        
-
-        //Use Lat/Lon to get weather data for searched city name
-        function getWeatherData() {
-            weatherDataUrl = 'https://api.openweathermap.org/data/3.0/onecall?lat=' + lat + '&lon=' + lon + '&units=imperial&appid=' + weatherApiKey;
-            fetch(weatherDataUrl)
-                .then(function (response) {
-                    if (response.ok) {
-                        response.json().then(function (data) {
-                            currentTemp = data.current.temp;
-                            currentWind = data.current.wind_speed;
-                            currentHumidity = data.current.humidity;
-
-                            // currentWeatherIcon = data.current.weather.icon; TODO (Undefined)
-                            showCurrentWeather();
-                            console.log(data);
-                        })
-                    }
-                })
-        //Set variable to determine that search function has already been run
-        searchRun = true;
-        //Check if search has been run before, if so, update recent search list 
-        checkSearch();
-        }
+//Check is search has already been executed, if so, update recent search list
+function checkSearch() {
+    if (searchRun === true) {
+        recentSearches.replaceChildren('');
+        currentCitySection.replaceChildren('');
+        fiveDayHeader.replaceChildren('');
+        fiveDayList.replaceChildren('');
+        showSearchHistory();
+    }
 }
+
+//Get city from search
+function getCity() {
+
+    city = document.getElementById('city').value;
+
+    //If search is empty, alert user to enter a city
+    if (!city) {
+        window.alert('You must enter a city name to run a search');
+    } else if (isNaN(city)) {
+
+        //Clear search
+        $("#search-form")[0].reset();
+
+        //Get data from APIs function
+        getAPIData();
+
+    } else {
+        window.alert('You can not enter numbers into this form');
+    }
+}
+
+function saveCity() {
+    //Add city to beginning of recentCities array and save to LocalStorage
+    recentCities.unshift(city);
+    localStorage.setItem('recentCities', JSON.stringify(recentCities));
+}
+
+//Get weather data from APIs
+function getAPIData() {
+    //Openweather API Url based on city search input
+    geocodingUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=1&appid=' + weatherApiKey;
+
+    //Get Lat/Lon from geocoding API
+    fetch(geocodingUrl)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                lat = data[0].lat;
+                lon = data[0].lon;
+
+                //Send Lat/Lon coordinates to API for city results
+                getWeatherData();
+                })
+            }
+        });     
+
+    //Use Lat/Lon to get weather data for searched city name
+    function getWeatherData() {
+    weatherDataUrl = 'https://api.openweathermap.org/data/3.0/onecall?lat=' + lat + '&lon=' + lon + '&units=imperial&appid=' + weatherApiKey;
+
+    //Get weather data for city
+    fetch(weatherDataUrl)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+
+                    //Current weather data
+                    currentTemp = data.current.temp;
+                    currentWind = data.current.wind_speed;
+                    currentHumidity = data.current.humidity;
+
+                    //Forecasted weather data
+                    dailyTemp = [data.daily[0].temp.day, data.daily[1].temp.day, data.daily[2].temp.day, data.daily[3].temp.day, data.daily[4].temp.day];
+                    dailyWind = [data.daily[0].wind_speed, data.daily[1].wind_speed, data.daily[2].wind_speed, data.daily[3].wind_speed, data.daily[4].wind_speed];
+                    dailyHumidity = [data.daily[0].humidity, data.daily[1].humidity, data.daily[2].humidity, data.daily[3].humidity, data.daily[4].humidity];
+
+                    // currentWeatherIcon = data.current.weather.icon; TODO (Undefined)
+                    showCurrentWeather();
+
+                    //Save city to history Array
+                    saveCity();
+
+                })
+            }
+        })
+
+    //Set variable to designate that search function has already been run
+    searchRun = true;
+
+    //Check if search has been run before, if so, update recent search list 
+    checkSearch();
+    }
+}
+
 
 //Fill in current weather section with current weather conditions
 function showCurrentWeather() {
-
     //Current city heading with date & weather icon
     var showCity = document.createElement('h3');
     showCity.innerText = city + ' (' + date + ')' + currentWeatherIcon;
@@ -110,14 +140,13 @@ function showCurrentWeather() {
         for ( var i = 1; i < 6; i++) {
             var nextDate = dayjs().add([i], 'day').format('M/DD/YYYY');
             var fiveDayDiv = document.createElement('div');
-        fiveDayDiv.innerHTML = "<div id='fivedaydiv'><h3>" + nextDate + "</h3>\n" + showCurrentData.innerHTML + "</div>";
+        fiveDayDiv.innerHTML = "<div id='fivedaydiv'><h3>" + nextDate + "</h3>\n<ul><li>Temp: " + dailyTemp[i-1] + "  \u00B0F</li>\n<li>Wind: " + dailyWind[i-1] + " MPH</li>\n<li>Humidity: " + dailyHumidity[i-1] + " %</li></ul></div>";
         fiveDayList.appendChild(fiveDayDiv);
         }
 }
 
 //Get recent cities from LocalStorage to display as recent search list
 function showSearchHistory() {
-    //Get search history from Local Storage (up to 10)
     pulledHistory = localStorage.getItem('recentCities');
 
     //If no search history, tell user there is no recent search history
@@ -126,31 +155,33 @@ function showSearchHistory() {
         noHistory.innerText = 'No recent Searches';
         recentSearches.append(noHistory);
 
-    //If there is history, get it, parse array, run a loop to create buttons for each city in recent search history
+    //If there is history, parse array, run a loop to create buttons for each city in recent search history, use name attribute to pass to search functions
     } else {
         recentCities = JSON.parse(pulledHistory);
         var recentCitiesSliced = recentCities.slice(0, 10);
         for (i = 0; i < recentCitiesSliced.length; i++) {
-            var historyListItem = document.createElement('button');
-            var historyName = recentCitiesSliced[i];
+            historyListItem = document.createElement('button');
+            historyListItem.name = 'history-button';
+            historyListItem.id = recentCitiesSliced[i];
+            historyName = recentCitiesSliced[i];
             historyListItem.innerText = historyName;
             recentSearches.append(historyListItem);
-           // historyListItem.setAttribute('click', search(city)); TODO
         }
+    }   
+      
+    //Get city from history button selected
+    var buttons = document.getElementsByName("history-button");
+    var buttonPressed = e => {
+        city = e.target.id;
+        console.log(city);
+        searchRun === true;
+        getAPIData();
+    }
+
+    for (let button of buttons) {
+        button.addEventListener('click', buttonPressed);
     }
 }
 
-//Check is search has already been executed, if so, update recent search list
-function checkSearch() {
-    if (searchRun === false) {
-        showSearchHistory();
-    } else if (searchRun === true) {
-        recentSearches.replaceChildren('');
-        currentCitySection.replaceChildren('');
-        fiveDayHeader.replaceChildren('');
-        fiveDayList.replaceChildren('');
-        showSearchHistory();
-        //Remove previous city data and replace with new search data
-
-    }
-}
+//Show search history on page load
+showSearchHistory();
